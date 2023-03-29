316680d8df26bad51aa81ed23dbd1a259527a989
Aryonna Rice <77810538+aryonna-rice@users.noreply.github.com>
2023-03-29 14:08:19 -0400

    Created event, club, and user models and entities (#37)

---
+++ backend/models/event_attendence.py
@@ -0,0 +1,6 @@
+"""Event attendence model serves as the data object for representing a registered user for an event."""
+from pydantic import BaseModel
+
+class Event_Attendence(BaseModel):
+    eventId: int
+    userId: int
