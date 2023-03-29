ceb636118d240db416c27930df603d73271dc1c4
Catherine Roberts <crobert7@email.unc.edu>
2023-03-29 11:28:50 -0400

    Create EventsComponent class to display on -All Events- page

--- frontend/src/app/events/events.component.ts
+++ frontend/src/app/events/events.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-events',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
+  // Use an observable class so that the event data can be synchronous with the database
+  // TODO: create and import an Events class
+  public events$ = Observable<Event[]>
+
+  // TODO: create and import eventService
+  constructor(eventService: EventService, private http: HttpClient) {
+    // TODO: create HTTP getEvents() method
+    this.events$ = eventService.getEvents()
+  }
+
+  // This delete method will be callable by Club Leaders 
+  // TODO: implement authentication
+  onDelete(event: Event) {
+    // TODO: implement HTTP method to delete Events
+    // TODO: change 'routename' to correct path
+    this.http.delete<Event>('/api/routename' + Event).subscribe({
+      next: (x) => this.reload(),
+      error: (err) => this.onError(err)
+      
+    })
+  }
 
 }
