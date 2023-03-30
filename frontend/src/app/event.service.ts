55061b892557bfe17f23f9841c2d71be5a1513b0
Catherine Roberts <crobert7@email.unc.edu>
2023-03-30 15:46:07 -0400

    Create fake events to display and comment out HTTP requests

--- frontend/src/app/event.service.ts
+++ frontend/src/app/event.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+export interface EventObject {
+  id: number,
+  name: string,
+  location: string, 
+  description: string,
+  date: string,
+  club_id: number
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
 
-  constructor() { }
+export class EventService {
+  events: EventObject[] = []
+
+  constructor() {
+    // Add fake events for Student Story view
+    const event1: EventObject = {id: 0, "name": 'Event 1', "location": 'SN14', 'description': 'An event hosted by club X', "date": '04/05/23', "club_id": 4}
+    const event2: EventObject = {id: 1, "name": 'Event 2', "location": 'FB08', 'description': 'An event hosted by club Y', "date": '04/10/23', "club_id": 5}
+    this.events.push(event1)
+    this.events.push(event2)
+  }
+
+  getAllEvents(): EventObject[] {
+    return this.events;
+  }
 }
---
+++ frontend/src/app/events/events.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EventObject, EventService } from '../event.service'
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent {
+  // Use an observable class so that the event data can be synchronous with the database
+  // TODO: create and import an Events class
+  //public events$ = Observable<Event[]>
+  public events: EventObject[]
+
+  // TODO: create and import eventService
+  constructor(eventService: EventService, private http: HttpClient) {
+    // TODO: create HTTP getEvents() method
+    this.events = eventService.getAllEvents()
+  }
+
+  // This delete method will be callable by Club Leaders 
+  // TODO: implement authentication
+  onDelete(event: Event) {
+    // TODO: implement HTTP method to delete Events
+    // TODO: change 'routename' to correct path
+    // this.http.delete<Event>('/api/routename/' + Event).subscribe({
+    //   next: (x) => this.reload(),
+    //   error: (err) => this.onError(err)
+
+    // })
+
+  }
+
+}
