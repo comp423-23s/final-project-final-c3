dc9781c6290e53566b813dc00a0671acf159e75b
Catherine Roberts <crobert7@email.unc.edu>
2023-03-31 14:24:32 -0400

    Add onDelete method to myEventsComponent

--- frontend/src/app/my-events/my-events.component.ts
+++ frontend/src/app/my-events/my-events.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Route } from '@angular/router';
 import { isAuthenticated } from '../gate/gate.guard';
 import { profileResolver } from '../profile/profile.resolver';
 import { Profile } from '../profile/profile.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -33,4 +33,24 @@ export class MyEventsComponent {
     this.my_events$ = eventService.getMyEvents()
   }
 
+  onDelete(event1: Event) {
+    let event = event1;
+    // how to pass in event1
+    this.http.delete<Event>('/api/event/delete').subscribe( {
+      next: (x) => this.reload(),
+      error: (err) => this.onError(err)
+    })
+  }
+
+  reload(): void {
+    this.my_events$ = this.http.get<Event[]>('/api/event/all')
+  }
+
+  private onError(err: HttpErrorResponse) {
+    if (err.error.detail) {
+      window.alert(err.error.detail);
+    } else {
+      window.alert("Unknown error: " + JSON.stringify(err));
+    }
+  }
 }
