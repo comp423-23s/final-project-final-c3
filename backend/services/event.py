2be3192c83a0d593a8049354ae167bccf54ace96
Aryonna Rice <77810538+aryonna-rice@users.noreply.github.com>
2023-04-01 11:52:34 -0400

    Update event.py

--- backend/services/event.py
+++ backend/services/event.py
@@ -12,28 +12,40 @@ class EventService:
         self._session = session
     
     def events_by_pid(self, pid: int) -> list[Event]:
-        stmt = text('SELECT * FROM events WHERE user_id = :pid')
-        result = self._session.execute(stmt, {'id': pid})
-        return result
+        """Get events user has registered for by user's pid."""
+        events: list[Event] = []
+        query = select(EventEntity).where(EventEntity.name != "None")
+        event_entities: list[ClubEntity] = self._session.scalar(query)
+        if event_entities is None:
+            return events
+        else:
+            for event in event_entities:
+                for attendee in event.attendees:
+                    if attendee.pid == pid:
+                        model = event.to_model()
+                        events.append(model)
+            return events
     
-    def get_all_events(self) -> list[Event] | None:
-        all_events = list[Event]
-        query = select(EventEntity).all()
+    def get_all_events(self) -> list[Event]:
+        """Get all registered events in the database."""
+        all_events: list[Event] = []
+        query = select(EventEntity).where(EventEntity.name != "None")
         event_entities: list[EventEntity] = self._session.scalar(query)
         if event_entities is None:
-            return None
+            return all_events
         else:
             for event in event_entities:
                 model = event.to_model()
                 all_events.append(model)
             return all_events
     
-    def display_events_by_club_id(self, club_id: int) -> list[Event] | None:
-        events = list[Event]
+    def display_events_by_club_id(self, club_id: int) -> list[Event]:
+        """Returns a list of all events the club has registered."""
+        events: list[Event] = []
         query = select(EventEntity).where(EventEntity.club_id == club_id)
         event_entities: list[EventEntity] = self._session.scalar(query)
         if event_entities is None:
-            return None
+            return events
         else:
             for event in event_entities:
                 model = event.to_model()
@@ -41,14 +53,41 @@ class EventService:
             return events
     
     def delete_event(self, event: Event) -> None:
+        """Deletes an event."""
         self._session.delete(event)
         self._session.commit()
         self._session.flush()
         
-    def add_by_pid_to_event(self, pid: int, event: Event) -> None:
-        attendees = event.attendees
-        attendees.append(UserService.get(pid))
+    def add_by_pid_to_event(self, pid: int, event_id: int) -> None:
+        """Add a user to an event."""
+        query = select(EventEntity).where(EventEntity.id == event_id)
+        event_entity: EventEntity = self._session.scalar(query)
+        if event_entity is None:
+            #HTTPException
+            return None
+        else:
+            event = event_entity.to_model()
+            attendees = event.attendees
+            attendees.append(UserService.get(pid))
+            event_entity.update(event)
+            self._session.commit()
+            self._session.flush()
         
-    def delete_by_pid_from_event(self, pid: int, event: Event) -> None:
-        attendees = event.attendees
-        attendees.remove(UserService.get(pid))
+    def delete_by_pid_from_event(self, pid: int, event_id: int) -> None:
+        """Degregister a user from an event."""
+        query = select(EventEntity).where(EventEntity.id == event_id)
+        event_entity: EventEntity = self._session.scalar(query)
+        if event_entity is None:
+            #HTTPException
+            return None
+        else:
+            event = event_entity.to_model()
+            attendees = event.attendees
+            for attendee in attendees:
+                if attendee.pid == pid:
+                    attendees.remove(attendee)
+                    event_entity.update(attendee)
+                    self._session.commit()
+                    self._session.flush()
+                    break
+    
