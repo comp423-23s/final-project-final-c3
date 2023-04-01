7bcab054d14ff7afc913eb8af2d07d68401fa19c
Aryonna Rice <77810538+aryonna-rice@users.noreply.github.com>
2023-04-01 12:25:23 -0400

    Update club.py

--- backend/services/club.py
+++ backend/services/club.py
@@ -13,11 +13,11 @@ class ClubService:
         
     def get_all_clubs(self) -> list[Club]:
         """Returns all registered clubs in the database."""
-        all_clubs = list[Club]
+        all_clubs: list[Club] = []
         query = select(ClubEntity).where(ClubEntity.name != "None")
         club_entities: list[ClubEntity] = self._session.scalar(query)
         if club_entities is None:
-            return None
+            return all_clubs
         else:
             for club in club_entities:
                 model = club.to_model()
@@ -27,11 +27,11 @@ class ClubService:
     
     def get_clubs_by_pid(self, pid: int) -> list[Club]:
         """Returns all clubs that a user is a member of."""
-        clubs = list[Club]
+        clubs: list[Club] = []
         query = select(ClubEntity).where(ClubEntity.name != "None")
         club_entities: list[ClubEntity] = self._session.scalar(query)
         if club_entities is None:
-            return None
+            return clubs
         else:
             for club in club_entities:
                 for member in club.members:
@@ -41,14 +41,17 @@ class ClubService:
             return clubs
     
     def add_user_to_club(self, pid: int, club_id: int) -> None:
-        """Adds a user to a club."""
+        """Adds a user to a club.""" 
         query = select(ClubEntity).where(ClubEntity.id == club_id)
         club_entity: ClubEntity = self._session.scalar(query)
         if club_entity is None:
-            return None
+            raise Exception("Club does not exist.")
         else:
             club = club_entity.to_model()
             members = club.members
+            for member in members:
+                if member.pid == pid:
+                    raise Exception("User already is a member of club.")
             members.append(UserService.get(pid))
             club_entity.update(club)
             self._session.commit()
@@ -60,7 +63,7 @@ class ClubService:
         query = select(ClubEntity).where(ClubEntity.id == club_id)
         club_entity: ClubEntity = self._session.scalar(query)
         if club_entity is None:
-            return None
+            return Exception("Club does not exist.")
         else:
             club = club_entity.to_model()
             members = club.members
@@ -70,7 +73,9 @@ class ClubService:
                     club_entity.update(club)
                     self._session.commit()
                     self._session.flush()
-                    break
+                    return
+           return Exception("User is not a member of club.")
+            
     
     
     
