b5aafc90d4e4c602d3e5efe58b583aaa8fa8bf58
Aryonna Rice <77810538+aryonna-rice@users.noreply.github.com>
2023-04-01 13:18:08 -0400

    Update club.py

--- backend/api/club.py
+++ backend/api/club.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from ..services import ClubService
 from ..models import User, Club
 from .authentication import registered_user
@@ -8,27 +8,36 @@ api = APIRouter(prefix="/api/club")
 # get all clubs
 @api.get("/all", response_model=list[Club], tags=['Club'])
 def get_all_clubs(club_svc: ClubService = Depends()):
-    all_clubs = club_svc.get_all_clubs()
-    if all_clubs is None:
-        return []
-    return all_clubs
+    try: 
+        all_clubs = club_svc.get_all_clubs()
+        return all_clubs
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 # get all clubs a user is in using their pid
 @api.get("/user/{pid}", response_model=list[Club], tags=['Club'])
 def get_clubs_by_pid(pid: int, club_svc: ClubService = Depends()):
-    user_clubs = club_svc.get_clubs_by_pid(pid)
-    if user_clubs is None:
-        return []
-    return user_clubs
+    try:
+        user_clubs = club_svc.get_clubs_by_pid(pid)
+        return user_clubs
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+        
 
 # Add user to club
-@api.put("/add/{club_id}/{pid}", response_model=User, tags=['Club'])
+@api.put("/add/{club_id}/{pid}", tags=['Club'])
 def add_user_to_club(club_id: int, pid: int, club_svc: ClubService = Depends()):
-    club_svc.add_user_to_club(pid, club_id)
-    return "User added to club."
+    try:
+        club_svc.add_user_to_club(pid, club_id)
+        return "OK"
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 # Remove user from club
-@api.delete("/remove/{club_id}", response_model=User, tags=['Club'])
+@api.delete("/remove/{club_id}", tags=['Club'])
 def remove_user_from_club(club_id: int, pid: int,club_svc: ClubService = Depends()):
-    club_svc.delete_user_from_club(pid, club_id)
-    return "User removed from club."
+    try:
+        club_svc.delete_user_from_club(pid, club_id)
+        return "OK"
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
