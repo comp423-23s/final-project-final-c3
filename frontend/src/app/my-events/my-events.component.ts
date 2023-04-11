d41374dd5f48823bd8ae01ad401e9704af241409
Catherine Roberts <crobert7@email.unc.edu>
2023-04-11 16:27:30 -0400

    Correctly Call Backend Routes in Frontend/Events (#101)
    * Create register/cancel button with empty functions to go with them
    
    * Change event component/service to look like club component/service
    
    * Correctly call getMyEvents()
    
    * Clean up my-events component. Add Cancel method
    
    * Fix add user to event method bug
    
    * Fix events by user method
    
    ---------
    
    Co-authored-by: Karen Gonzalez-Palomo <karengp8@ad.unc.edu>

--- frontend/src/app/my-events/my-events.component.ts
+++ frontend/src/app/my-events/my-events.component.ts
@@ -24,32 +24,30 @@ export class MyEventsComponent {
   };
   
   public my_events$: Observable<Event[]>
-  public profile: Profile
 
   constructor(private eventService: EventService, route: ActivatedRoute, private http: HttpClient, protected snackBar: MatSnackBar) {
-    this.eventService = eventService
-    const data = route.snapshot.data as { profile: Profile }
-    console.log(route.snapshot)
-    this.profile = data.profile
-    this.my_events$ = eventService.getMyEvents(this.profile.pid)
+    this.my_events$ = eventService.getMyEvents()
   }
 
-  changeStatus(event: Event) {
-    // TODO: implement
-    if (this.isUserInEvent(event)) {
-      this.onCancel(event)
-    }
-    else {
-      this.onRegister(event)
-    }
-  }
-
-  private onRegister(event: Event) {
-    this.snackBar.open("Successfully registered for " + event.name, "", { duration: 2000 })
-  }
-
-  private onCancel(event: Event) {
+  onCancel(event: Event) {
+    this.eventService.removeUserFromEvent(event).subscribe({
+      next: () => this.onSuccess(),
+      error: (err) => this.onError(err)
+    })
     this.snackBar.open("Successfully cancelled registration for " + event.name, "", { duration: 2000 })
+  }
+
+  onSuccess(): void {
+    this.my_events$ = this.eventService.getMyEvents()
+  }
+
+  onError(err: Error) : void{
+    if (err.message) {
+      console.log(err)
+      window.alert("The error is: " + err.message);
+    } else {
+      window.alert("Unknown error: " + JSON.stringify(err));
+    }
   }
 
   // Function to determine whether or not a student is part of an event
@@ -57,25 +55,4 @@ export class MyEventsComponent {
     // Delegate to the service. 
     return this.eventService.isUserInEvent(event)
   }
-
-  // onDelete(event1: Event) {
-  //   let event = event1;
-  //   // how to pass in event1
-  //   this.http.delete<Event>('/api/event/delete').subscribe( {
-  //     next: (x) => this.reload(),
-  //     error: (err) => this.onError(err)
-  //   })
-  // }
-
-  // reload(): void {
-  //   this.my_events$ = this.eventService.getMyEvents(this.profile.pid)
-  // }
-
-  // private onError(err: HttpErrorResponse) {
-  //   if (err.error.detail) {
-  //     window.alert(err.error.detail);
-  //   } else {
-  //     window.alert("Unknown error: " + JSON.stringify(err));
-  //   }
-  // }
 }
