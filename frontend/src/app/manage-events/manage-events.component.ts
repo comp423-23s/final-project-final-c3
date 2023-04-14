fad85721e4859311d05ce2c968b9190beec77cfc
Catherine Roberts <crobert7@email.unc.edu>
2023-04-14 10:35:18 -0400

    Add route named /manageevents to component

--- frontend/src/app/manage-events/manage-events.component.ts
+++ frontend/src/app/manage-events/manage-events.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Route } from '@angular/router';
+import { isAuthenticated } from '../gate/gate.guard';
+import { profileResolver } from '../profile/profile.resolver';
 
 @Component({
   selector: 'app-manage-events',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-events.component.css']
 })
 export class ManageEventsComponent {
-
+  public static Route: Route = {
+    path: 'manageevents',
+    component: ManageEventsComponent, 
+    title: 'Manage Events', 
+    canActivate: [isAuthenticated], 
+    resolve: { profile: profileResolver }
+  };
 }
