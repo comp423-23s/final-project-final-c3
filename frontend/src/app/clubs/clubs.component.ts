08a4523deb1214fa0fdbef5e345c64241e011fc6
QianqianHong <77793476+QianqianHong@users.noreply.github.com>
2023-04-14 21:47:13 -0400

    Enable auto-updating profile (#122)
    * Add Example Folder with Sql.ORM Examples
    
    * Delete examples folder because branch staging is needed
    
    * Add folder to store example code we can reference to
    
    * Add another database example with a Python class
    
    * Add changes
    
    * Enable auto-updating profile in components

--- frontend/src/app/clubs/clubs.component.ts
+++ frontend/src/app/clubs/clubs.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { Observable, map, of } from 'rxjs';
 import { ActivatedRoute, Route } from '@angular/router'
 import { isAuthenticated } from 'src/app/gate/gate.guard';
-import { Profile } from '../profile/profile.service'
+import { Profile, ProfileService } from '../profile/profile.service'
 import { Club, ClubsService, User_Club } from '../clubs.service';
 import { profileResolver } from '../profile/profile.resolver';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { throwToolbarMixedModesError } from '@angular/material/toolbar';
+import { ProfileEditorComponent } from '../profile/profile-editor/profile-editor.component';
 
+@Injectable({
+  providedIn: 'root'
+})
 @Component({
   selector: 'app-clubs',
   templateUrl: './clubs.component.html',
@@ -25,12 +29,15 @@ export class ClubsComponent {
   };
 
   public profile: Profile
+  public profile$: Observable<Profile | undefined>
   public clubs$: Observable<Club[]>
   public user_clubs$: Observable<User_Club[]>
+  // static profile: Profile;
 
-  constructor(route: ActivatedRoute, private clubsService: ClubsService, protected snackBar: MatSnackBar) {
+  constructor(route: ActivatedRoute, private clubsService: ClubsService, protected snackBar: MatSnackBar, private profileService: ProfileService) {
     const data = route.snapshot.data as { profile: Profile }
     this.profile = data.profile
+    this.profile$ = profileService.profile$
     this.clubs$ = clubsService.getAllClubs()
     this.clubs$ = this.clubs$.pipe(map((clubs: Club[]) => {return clubs.map(club => {return {...club, show_short_description: true}})}))
     this.user_clubs$ = this.clubs$.pipe(map((clubs: Club[]) => {
@@ -59,10 +66,20 @@ export class ClubsComponent {
 
   // Enables a student to join a club
   onJoin(club: Club): void {
+    this.profileService.http.get<Profile>('/api/profile').subscribe(
+      {
+        next: (data) => {this.onSuccessUpdateProfile(data)},
+        error: (err) => console.log(err)
+      }
+    )
     this.clubsService.joinClub(club).subscribe({
       next: () => this.onSuccess(),
       error: (err) => this.onError(err)
     })
+  }
+
+  private onSuccessUpdateProfile(profile: Profile): void {
+    this.profile = profile
   }
 
   // Enables a student to leave a club
