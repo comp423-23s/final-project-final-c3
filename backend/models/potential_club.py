4dac1b67a0983d61805f25ecb5c8ef2019f5c276
Aryonna Rice <aryonnarice@unc.edu>
2023-04-18 18:03:13 -0400

    Added week day time model and made changes to potentialclub and club.

--- backend/models/potential_club.py
+++ backend/models/potential_club.py
@@ -2,11 +2,14 @@
 
 from pydantic import BaseModel
 from .user import User
+from .week_day_time import WeekDayTime
 
 class PotentialClub(BaseModel):
     id: int | None = None
     name: str
     description: str
     founder_id: int
+    meeting_times: list['WeekDayTime'] = []
+    catagories: list[str] = []
 
 PotentialClub.update_forward_refs()
