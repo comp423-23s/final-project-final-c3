095fd10446896f524d7ebd85813554248635cfaa
Catherine Roberts <crobert7@email.unc.edu>
2023-04-19 23:10:24 -0400

    Added way to get date input for start and end

--- frontend/src/app/register-event/register-event.component.ts
+++ frontend/src/app/register-event/register-event.component.ts
@@ -24,20 +24,20 @@ export class RegisterEventComponent {
   
   }
 
-  onSubmitEvent(club_code: string, name: string, description: string, location: string): void {
+  onSubmitEvent(club_code: string, name: string, description: string, location: string, start: string, end: string): void {
     // TODO: change parameters to right types
     let club_id = this.getClubID(club_code)
-    let date_number: number = Date.now()
-    let date: Date = new Date(date_number)
-    
+    let start_time: Date = new Date(start)
+    let end_time: Date = new Date(end)
+    console.log(start_time)
     var potentialEvent: PotentialEvent = {
       id: null,
       club_id: club_id,
       name: name,
       description: description,
       location: location,
-      start_date: date,
-      end_date: date
+      start_date: start_time,
+      end_date: end_time
     }    
 
     this.eventService.createNewEvent(potentialEvent).subscribe(
