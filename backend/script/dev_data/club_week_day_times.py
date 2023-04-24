6ae320dbbf0b22e098733994cc65af497ba57d7c
QianqianHong <jeanhong@ad.unc.edu>
2023-04-24 15:56:32 -0400

    Add time and categories for pre-set clubs

---
+++ backend/script/dev_data/club_week_day_times.py
@@ -0,0 +1,16 @@
+"""club/categories pairings."""
+
+from . import clubs
+from . import week_day_times
+
+__authors__ = ["Kris Jordan"]
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
+
+pairs = [
+    (clubs.club_a, week_day_times.club_a_time),
+    (clubs.club_b, week_day_times.club_b_time),
+    (clubs.club_c, week_day_times.club_c_time),
+    (clubs.club_d, week_day_times.club_d_time),
+    (clubs.club_e, week_day_times.club_e_time),
+]
