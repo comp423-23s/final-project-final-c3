07a52e2cb5a2a59db62ac827002086b882c10451
QianqianHong <77793476+QianqianHong@users.noreply.github.com>
2023-04-28 22:57:13 -0400

    Add Root User's Leader and Admin Roles (#252)
    * Add Example Folder with Sql.ORM Examples
    
    * Delete examples folder because branch staging is needed
    
    * Add folder to store example code we can reference to
    
    * Add another database example with a Python class
    
    * Add changes
    
    * Add root user with leader and admin roles

--- backend/script/dev_data/user_roles.py
+++ backend/script/dev_data/user_roles.py
@@ -9,6 +9,8 @@ __license__ = "MIT"
 
 pairs = [
     (users.root, roles.student),
+    (users.root, roles.leader),
+    (users.root, roles.administrator),
     (users.merritt_manager, roles.leader),
     (users.arden_ambassador, roles.administrator),
     (users.ally_ambassador, roles.administrator)
